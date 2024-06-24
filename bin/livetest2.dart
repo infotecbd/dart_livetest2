e928734993c9cbfac8696c7388e29a905fe46c48
infotecbd <mdhossain.odesk@gmail.com>
2024-06-24 23:21:05 +0600

    Ostad Dart Live Test2
    Ostad Dart Live Test2

---
+++ bin/livetest2.dart
@@ -0,0 +1,24 @@
+class Media {
+  void play() {
+    print('Playing media...');
+  }
+}
+
+// Derrived class song
+class Song extends Media {
+  String artist;
+
+  Song(this.artist);
+
+  @override
+  void play() {
+    print('Playing song by $artist...');
+  }
+}
+void main() {
+  Media media = Media();
+  media.play();
+
+  Song song = Song('James');
+  song.play();
+}
